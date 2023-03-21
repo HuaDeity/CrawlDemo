65a6df03437286a171688452de5b1f5ab49051c7
HuaDeity <64831499+HuaDeity@users.noreply.github.com>
2023-03-22 00:21:57 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # CrawlDemo
+
+This is a demo of image crawling.
+
+## Features
+
+Automate web crawling by retrieving images from gettyimages.com and flipping pages automatically.
+
+## Installation
+
+### Requirements
+
+Python 3.7+  
+Chrome/Firefox/Edge/Safari
+
+### Install
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+cd millitary
+scrapy crawl gettyimages -a search_term=aircraftcarrier -a page_number=3 -a browser=chrome
+```
+
+To retrieve a specific image, the user needs to provide three pieces of information: the search term (keyword), the desired page number, and their preferred web browser**(chrome/firefox/edge/safari)**.
+
+## Contributing
+
+Contributions are welcome! Please refer to our CONTRIBUTING.md for details on how to contribute.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/HuaDeity/CrawlDemo/blob/main/LICENSE) file for details.
+
+## Contact
+
+Yizun Wang  
+[Email](mailto:wangyizun@mail.nwpu.edu.cn)
---
+++ millitary/millitary/spiders/gettyimages.py
@@ -0,0 +1,91 @@
+import scrapy
+import requests
+import os
+import shutil
+from selenium import webdriver
+from selenium.webdriver.safari.options import Options as SafariOptions
+from scrapy_selenium import SeleniumRequest
+from scrapy.selector import Selector
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+os.environ['WDM_LOCAL'] = '1'
+
+class GettyimagesSpider(scrapy.Spider):
+    name = "gettyimages"
+    allowed_domains = ["gettyimages.com"]
+    start_urls = ["https://gettyimages.com/"]
+
+    def __init__(self, search_term=None, page_number=None, browser=None, **kwargs):
+        # 设置搜索关键词
+        self.search_term = search_term
+        self.page_number = page_number
+        if browser == "chrome":
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless=new')
+            options.add_argument('--start-maximized')
+            options.add_argument('--disable-extensions')
+            options.add_argument('--disable-gpu')
+            options.add_argument('--no-sandbox')
+            self.driver = webdriver.Chrome(chrome_options=options)
+        if browser == 'firefox':
+            options = webdriver.FirefoxOptions()
+            options.add_argument('-headless')
+            self.driver = webdriver.Firefox(options=options)
+        if browser == 'edge':
+            options = webdriver.EdgeOptions()
+            options.add_argument('--headless=new')
+            options.add_argument('--start-maximized')
+            options.add_argument('--disable-extensions')
+            options.add_argument('--disable-gpu')
+            options.add_argument('--no-sandbox')
+            self.driver = webdriver.Edge(options=options)
+        if browser == 'safari':
+            options = SafariOptions()
+            self.driver = webdriver.Safari(options=options)
+        
+        super().__init__(**kwargs)
+
+    def start_requests(self):
+        # 模拟搜索操作
+        url = f'https://gettyimages.com/photos/{self.search_term}'
+        yield SeleniumRequest(
+            url=url,
+            callback=self.parse,
+            meta={'driver': self.driver},
+            wait_time=10
+        )
+
+    def parse(self, response):
+        driver = response.meta['driver']
+        driver.get(response.url)
+        page = int(self.page_number)
+        image_urls = []
+        while page > 0:
+            page_source = driver.page_source
+            selector = Selector(text=page_source)
+            images = selector.xpath("//img")
+            for image in images:
+                src = image.xpath("@src").get()
+                if src and "https://media.gettyimages.com/" in src:
+                    image_urls.append(src)
+            button = driver.find_element(By.XPATH, "//button[@data-testid='pagination-button-next' and @type='button']")
+            driver.execute_script("arguments[0].click();", button)
+            sleep(3)
+            page -= 1
+        driver.quit()
+
+        if not os.path.exists(f"images/{self.search_term}"):
+                os.makedirs(f"images/{self.search_term}")
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15'
+        }
+        for image_url in image_urls:
+            image_response = requests.get(image_url, headers=headers, stream=True)
+            file_name = image_url.split("/")[-1].split("?")[0]
+            file_name = os.path.join(f"images/{self.search_term}", file_name)
+            with open(file_name, "wb") as f:
+                shutil.copyfileobj(image_response.raw, f)
+    
